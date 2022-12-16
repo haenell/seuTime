91d9d4a8f0379ec4bc04a0d2ff490dd49eded5f3
Haenell Farias <96090491+haenell@users.noreply.github.com>
2022-12-16 12:40:00 -0300

    Update Banner.js

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,7 +1,6 @@
 import "./Banner.css";
 
 function Banner() {
-  //JSX -- Parece com HTML mas não é
   return (
     <header className="banner">
       <img src="/valorant-wallpaper-15.jpg" alt=""></img>
